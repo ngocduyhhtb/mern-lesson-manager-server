b547625e41e043459b570b93b49e0d3150c6deb7
ngocduyhhtb <duy191203607@st.utc.edu.vn>
2021-05-07 13:44:43 +0700

    fix index.js

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const mongoose = require("mongoose");
 const authRouter = require('./routes/auth');
 const postRouter = require('./routes/post');
 const cors = require('cors');
+app.use(cors({
+    origin: "https://mern-client-d9084.web.app"
+}));
 const connectDB = async () => {
     try {
         await mongoose.connect(
@@ -25,7 +28,6 @@ const connectDB = async () => {
 connectDB().then();
 const PORT = 5000;
 app.use(express.json());
-app.use(cors());
 app.all('/', (req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "X-Requested-With");
