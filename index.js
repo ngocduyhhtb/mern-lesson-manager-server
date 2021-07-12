b27db49ea4e159acd4bee0e453d8e3f72436cff5
ngocduyhhtb <duy191203607@st.utc.edu.vn>
2021-07-12 12:41:07 +0700

    optimize

--- index.js
+++ index.js
@@ -1,45 +1,48 @@
-require('dotenv').config();
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const authRouter = require('./routes/auth');
-const postRouter = require('./routes/post');
-const cors = require('cors');
-app.use(cors({
-    origin: "https://mern-client-d9084.web.app"
-}));
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "https://mern-client-d9084.web.app"); // update to match the domain you will make the request from
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
-const connectDB = async () => {
-    try {
-        await mongoose.connect(
-            `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@mern-stack.ay9y6.mongodb.net/MERN-Stack?retryWrites=true&w=majority`,
-            {
-                useCreateIndex: true,
-                useNewUrlParser: true,
-                useUnifiedTopology: true,
-                useFindAndModify: false,
-            }
-        );
-        console.log("Mongoose connected");
-    } catch (error) {
-        console.log(error.message);
-        process.exit(-1);
-    }
-};
-connectDB().then();
+const authRouter = require("./routes/auth");
+const postRouter = require("./routes/post");
+const cors = require("cors");
 const PORT = 5000;
 app.use(express.json());
-app.all('/', (req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
-    next()
-});
-app.use('/api/auth', authRouter);
-app.use('/api/posts', postRouter);
-app.listen(5000, () => {
-    console.log(`App listening on port ${PORT}`);
+app.use(
+  cors({
+    origin: [
+      "http://localhost:3000",
+      "https://mern-lesson-manager-client.vercel.app",
+    ],
+  })
+);
+// app.use(function (req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+//   next();
+// });
+const connectDB = async () => {
+  try {
+    await mongoose.connect(
+      `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@mern-stack.ay9y6.mongodb.net/MERN-Stack?retryWrites=true&w=majority`,
+      {
+        useCreateIndex: true,
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false,
+      }
+    );
+    console.log("Mongoose connected");
+  } catch (error) {
+    console.log(error.message);
+    process.exit(-1);
+  }
+};
+connectDB();
+app.use("/api/auth", authRouter);
+app.use("/api/posts", postRouter);
+app.listen(process.env.PORT || PORT, () => {
+  console.log(`App listening on port ${PORT}`);
 });
--- routes/auth.js
+++ routes/auth.js
@@ -9,91 +9,91 @@ const verifyToken = require("../middleware/auth");
 // @desc Check if user is logged in
 // @access Public
 router.get("/", verifyToken, async (req, res) => {
-    try {
-        const user = await User.findById(req.userId).select("-password");
-        if (!user) {
-            return res
-                .status(400)
-                .json({success: false, message: "User not found"});
-        }
-        res.status(200).json({success: true, user});
-    } catch (error) {
-        console.log(error);
-        return res
-            .status(500)
-            .json({success: false, message: "Internal server error"});
+  try {
+    const user = await User.findById(req.userId).select("-password");
+    if (!user) {
+      return res
+        .status(400)
+        .json({ success: false, message: "User not found" });
     }
+    res.status(200).json({ success: true, user });
+  } catch (error) {
+    console.log(error);
+    return res
+      .status(500)
+      .json({ success: false, message: "Internal server error" });
+  }
 });
 // @router POST api/auth/register
 // @desc Register user
 // @access Public
 router.post("/register", async (req, res) => {
-    const {username, password} = req.body;
-    if (!username || !password) {
-        return res
-            .status(400)
-            .json({success: false, message: "Missing username and/or password"});
+  const { username, password } = req.body;
+  if (!username || !password) {
+    return res
+      .status(400)
+      .json({ success: false, message: "Missing username and/or password" });
+  }
+  try {
+    const user = await User.findOne({ username });
+    if (user) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Username already taken" });
     }
-    try {
-        const user = await User.findOne({username});
-        if (user) {
-            return res
-                .status(400)
-                .json({success: false, message: "Username already taken"});
-        }
-        //All good
-        const hashPassword = await argon2.hash(password);
-        const newUser = new User({username, password: hashPassword});
-        await newUser.save();
-        const accessToken = jwt.sign(
-            {userId: newUser._id},
-            process.env.ACCESS_TOKEN_SECRET
-        );
-        return res
-            .status(200)
-            .json({success: true, message: "Register success", accessToken});
-    } catch (error) {
-        console.log(error.message);
-        return res
-            .status(500)
-            .json({success: false, message: "Internal server error"});
-    }
+    //All good
+    const hashPassword = await argon2.hash(password);
+    const newUser = new User({ username, password: hashPassword });
+    await newUser.save();
+    const accessToken = jwt.sign(
+      { userId: newUser._id },
+      process.env.ACCESS_TOKEN_SECRET
+    );
+    return res
+      .status(200)
+      .json({ success: true, message: "Register success", accessToken });
+  } catch (error) {
+    console.log(error.message);
+    return res
+      .status(500)
+      .json({ success: false, message: "Internal server error" });
+  }
 });
 router.post("/login", async (req, res) => {
-    const {username, password} = req.body;
-    if (!username || !password) {
-        return res
-            .status(400)
-            .json({success: false, message: "Missing username and/or password"});
+  const { username, password } = req.body;
+  if (!username || !password) {
+    return res
+      .status(400)
+      .json({ success: false, message: "Missing username and/or password" });
+  }
+  try {
+    const user = await User.findOne({ username });
+    if (!user) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Incorrect username or password" });
     }
-    try {
-        const user = await User.findOne({username});
-        if (!user) {
-            return res
-                .status(400)
-                .json({success: false, message: "Incorrect username or password"});
-        }
-        //Username found
-        const passwordValid = await argon2.verify(user.password, password);
-        if (!passwordValid) {
-            return res
-                .status(400)
-                .json({success: false, message: "Incorrect username or password"});
-        }
-        //All Good
-        const accessToken = jwt.sign(
-            {userId: user._id},
-            process.env.ACCESS_TOKEN_SECRET
-        );
-        return res
-            .status(200)
-            .json({success: true, message: "LoginForm successfully", accessToken});
-    } catch (error) {
-        console.log(error.message);
-        return res
-            .status(500)
-            .json({success: false, message: "Internal server error"});
+    //Username found
+    const passwordValid = await argon2.verify(user.password, password);
+    if (!passwordValid) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Incorrect username or password" });
     }
+    //All Good
+    const accessToken = jwt.sign(
+      { userId: user._id },
+      process.env.ACCESS_TOKEN_SECRET
+    );
+    return res
+      .status(200)
+      .json({ success: true, message: "LoginForm successfully", accessToken });
+  } catch (error) {
+    console.log(error.message);
+    return res
+      .status(500)
+      .json({ success: false, message: "Internal server error" });
+  }
 });
 
 module.exports = router;
